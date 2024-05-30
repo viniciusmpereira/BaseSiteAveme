0023e41aadbc8d8c446d8a532de96296978459b9
Vinicius Pereira <91560956+viniciusmpereira@users.noreply.github.com>
2024-05-29 22:52:01 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://base-site-aveme-r1z4lbu53-aveme.vercel.app/',
   integrations: [mdx(), sitemap(), react(), tailwind()],
   output: "server",
   adapter: vercel()
